6252a1ff7699f60d373001968e115907eac6b749
adjmedina <adjmedina@gmail.com>
2025-01-26 22:12:04 -0600

    mejorado el reto de variables

---
+++ 01-intro/reto/go.mod
@@ -0,0 +1,8 @@
+module ejemplovariables
+
+go 1.23.5
+
+require (
+	github.com/go-resty/resty/v2 v2.16.5 // indirect
+	golang.org/x/net v0.34.0 // indirect
+)
--- 01-intro/reto/main.go
+++ 01-intro/reto/main.go
@@ -1,15 +1,38 @@
 // Paquete al que corresponde el conjunto de instrucciones
 package main
 
-// importacion del las liberias requeridas entre comillas
-import "fmt"
+// Todos los nombres de paquetes que se importan en el código deberán ir entre comillas dobles
+// Si un paquete aue se importe no se utiliza, el código no compilará
+import (
+	"fmt"
+	"os"
+	"time"
 
-// Funcion Principal o entrypoint
+	"github.com/go-resty/resty/v2"
+)
+
+// obtenerTipoDeCambio obtiene el tipo de cambio de USD a MXN
+// Entradas: ninguna
+// Salidas: string con el tipo de cambio o error
+func obtenerTipoDeCambio() (string, error) {
+	client := resty.New()
+	client.SetTimeout(10 * time.Second)
+	resp, err := client.R().Get("https://api.exchangeratesapi.io/latest?base=USD&symbols=MXN")
+	if err != nil {
+		return "", err
+	}
+
+	return resp.String(), nil
+}
+
+// Función Principal o entrypoint
+
 func main() {
+	resultado, err := obtenerTipoDeCambio()
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 
-	//Declaracion de variables requeridas
-	var precio float32 = 1.18
-
-	fmt.Printf("Probando una variable flotante %.2f", precio)
-
+	fmt.Printf("El producto tiene un valor de $s  al valor de cambio del dia de hoy", resultado)
 }
